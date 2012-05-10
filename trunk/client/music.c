07d8f676042c723ff340b7945dfaacd5f7eb61f2
alepulver <alepulver@users.sourceforge.net>
2012-05-10 23:46:30 +0000

    - Really fix music pause
    git-svn-id: https://svn.code.sf.net/p/quake2xp/code@225 d83f5309-02f3-4a42-a5f4-12e6af65da40

--- trunk/client/music.c
+++ trunk/client/music.c
@@ -70,7 +70,7 @@ void Music_Shutdown(void) {
 }
 
 // only to be called inside Music_Play
-static qboolean Music_PlayFile(const char *name, qboolean hasExt, qboolean paused) {
+static qboolean Music_PlayFile(const char *name, qboolean hasExt) {
 	soundparams_t sp;
 
 	if (hasExt)
@@ -85,10 +85,7 @@ static qboolean Music_PlayFile(const char *name, qboolean hasExt, qboolean pause
 			Com_DPrintf(S_COLOR_GREEN "Music_Play: playing \"%s.%s\"\n", name, music_handle->ext);
 
 		S_Streaming_Start(sp.bits, sp.channels, sp.rate, s_musicvolume->value);
-		if (paused)
-            mstat = MSTAT_PAUSED;
-        else
-            mstat = MSTAT_PLAYING;
+        mstat = cl_paused->value ? MSTAT_PAUSED : MSTAT_PLAYING;
 		return true;
 	} else {
 		Com_Printf(S_COLOR_YELLOW "Music_Play: unable to load \"%s\"\n", name);
@@ -99,7 +96,6 @@ static qboolean Music_PlayFile(const char *name, qboolean hasExt, qboolean pause
 void Music_Play(void) {
 	int track, count;
 	char name[MAX_QPATH];
-    qboolean paused = (mstat == MSTAT_PAUSED);
 
 	Music_Stop();
 
@@ -121,7 +117,7 @@ void Music_Play(void) {
 
 		case MUSIC_CD_FILES:
 			Q_snprintfz(name, sizeof(name), "music/track%02i", track);
-			Music_PlayFile(name, false, paused);
+			Music_PlayFile(name, false);
 			break;
 
 		case MUSIC_OTHER_FILES:
@@ -134,7 +130,7 @@ void Music_Play(void) {
 				fsIndex = (fsIndex+1) % fsNumFiles;
 			count = fsNumFiles;
 			while (count-- > 0) {
-				if (Music_PlayFile(fsList[fsIndex], true, paused))
+				if (Music_PlayFile(fsList[fsIndex], true))
 					return;
 				fsIndex = (fsIndex+1) % fsNumFiles;
 			}
