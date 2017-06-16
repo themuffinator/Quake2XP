d4b91ecb8097e15503d0a97934b737ce463d585b
kirkbarnes <kirkbarnes@users.sourceforge.net>
2017-06-16 17:14:27 +0000

    *steam launcher for console version - unicode and non unicode build for win32 only non unicode
    git-svn-id: https://svn.code.sf.net/p/quake2xp/code@929 d83f5309-02f3-4a42-a5f4-12e6af65da40

--- trunk/steamLauncher/main.cpp
+++ trunk/steamLauncher/main.cpp
@@ -1,19 +1,78 @@
-// steam hack.cpp : Defines the entry point for the console application.
-//
+/*==============================================
+quake2xp steam(tm) launcher
+console version support unicode and non unicode
+win32 support non unicode only
+==============================================*/
 
 #include "windows.h"
 
-#pragma warning(disable : 4996)		// vs 2005 security warnings
 
-wchar_t *convertCharArrayToLPCWSTR(const char* charArray)
+#ifdef UNICODE
+wchar_t *convertCharArrayToLPTSTR(const char* charArray)
 {
 	wchar_t* wString = new wchar_t[4096];
 	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
 	return wString;
 }
 
+/*
+// TODO !!! LPTSTR to CHAR
+LPWSTR pwz = "some text";
+char* psz = new char [wcslen(pwsz) + 1];
+
+wsprintfA ( psz, "%S", pwsz);
+*/
+#endif
+
 int main(int argc, char *argv[])
 {
+STARTUPINFO StartupInfo;
+PROCESS_INFORMATION ProcessInformation;
+
+StartupInfo.cb = sizeof(STARTUPINFO);
+StartupInfo.lpReserved = NULL;
+StartupInfo.lpDesktop = NULL;
+StartupInfo.lpTitle = NULL;
+StartupInfo.dwFlags = STARTF_USESHOWWINDOW;
+StartupInfo.wShowWindow = SW_SHOWNORMAL;
+StartupInfo.cbReserved2 = 0;
+StartupInfo.lpReserved2 = NULL;
+
+
+char cmd[256];
+strcpy_s(cmd, "quake2xp");
+strcat_s(cmd, " ");
+
+for (int i = 1; i < argc; i++)
+{
+strcat_s(cmd, argv[i]);
+strcat_s(cmd, " ");
+}
+
+#ifdef UNICODE
+
+LPTSTR szCmdline = convertCharArrayToLPTSTR(cmd);
+CreateProcess(NULL, szCmdline, NULL, NULL, TRUE, 0, NULL, NULL, &StartupInfo, &ProcessInformation);
+
+#else
+
+CreateProcess(NULL, (LPSTR)cmd, NULL, NULL, TRUE, 0, NULL, NULL, &StartupInfo, &ProcessInformation);
+
+#endif
+
+return 0;
+}
+
+// win32 version non unicode only
+#ifndef CONSOLE
+
+#ifndef UNICODE 
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+	if (hPrevInstance)
+		return 0;
+
 	STARTUPINFO StartupInfo;
 	PROCESS_INFORMATION ProcessInformation;
 
@@ -26,21 +85,16 @@ int main(int argc, char *argv[])
 	StartupInfo.cbReserved2 = 0;
 	StartupInfo.lpReserved2 = NULL;
 
-
 	char cmd[256];
-	strcpy(cmd, "quake2xp");
-	strcat(cmd, " ");
+	strcpy_s(cmd, "quake2xp");
+	strcat_s(cmd, " ");
+	strcat_s(cmd, lpCmdLine);
 
-	for (int i = 1; i < argc; i++)
-	{
-		strcat(cmd, argv[i]);
-		strcat(cmd, " ");
-	}
-		
-	LPTSTR szCmdline = convertCharArrayToLPCWSTR(cmd);
+	CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &StartupInfo, &ProcessInformation);
 
-	CreateProcess(NULL, szCmdline, NULL, NULL, TRUE, 0, NULL, NULL, &StartupInfo, &ProcessInformation);
-	
-	return 0;
+
+	return 1;
 }
 
+#endif
+#endif
