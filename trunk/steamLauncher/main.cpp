eb0b7faf98fa5fadbb480a2d56248ba332c63ed1
kirkbarnes <kirkbarnes@users.sourceforge.net>
2017-06-16 19:21:38 +0000

    *fix-fix steam launcher ))
    git-svn-id: https://svn.code.sf.net/p/quake2xp/code@931 d83f5309-02f3-4a42-a5f4-12e6af65da40

--- trunk/steamLauncher/main.cpp
+++ trunk/steamLauncher/main.cpp
@@ -1,14 +1,22 @@
 /*==============================================
-quake2xp steam(tm) launcher
-console version support unicode and non unicode
-win32 support non unicode only
+STEAM(TM) LAUNCHER FOR QUAKE2XP
+THIS CODE IS PART OF QUAKE2XP ENGINE
+THIS IS OPEN SOURCE SOFTWARE, IF YOU USE IT, 
+JUST LEAVE IN THE AUTHOR'S CREDITS ORIGINAL CODE
+
+CONSOLE VERSION SUPPORT UNICODE AND NON UNICODE
+WIN32 SUPPORT NON UNICODE ONLY
+WE CAN NOT JUST RENAME QUAKE2XP TO QUAKE2. VIDEO DRIVER 
+OF NVIDIA OR AMD WILL SEE THE NAME FROM THE LIST AND
+CUT OFF EXTENSION STRING TO CIRCUMVENT THE BUFFER 
+OVERFLOW ERROR IN THE CONSOLE.
 ==============================================*/
 
 #include "windows.h"
 
 
 #ifdef UNICODE
-wchar_t *convertCharArrayToLPTSTR(const char* charArray)
+wchar_t *convertCharToLPTSTR(const char* charArray)
 {
 	wchar_t* wString = new wchar_t[4096];
 	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
@@ -24,41 +32,39 @@ wsprintfA ( psz, "%S", pwsz);
 */
 #endif
 
-#ifdef CONSOLE
+#ifdef _CONSOLE
 
-int main(int argc, char *argv[])
-{
-STARTUPINFO StartupInfo;
-PROCESS_INFORMATION ProcessInformation;
+int main(int argc, char *argv[]){
 
-StartupInfo.cb = sizeof(STARTUPINFO);
-StartupInfo.lpReserved = NULL;
-StartupInfo.lpDesktop = NULL;
-StartupInfo.lpTitle = NULL;
-StartupInfo.dwFlags = STARTF_USESHOWWINDOW;
-StartupInfo.wShowWindow = SW_SHOWNORMAL;
-StartupInfo.cbReserved2 = 0;
-StartupInfo.lpReserved2 = NULL;
+	STARTUPINFO StartupInfo;
+	PROCESS_INFORMATION ProcessInformation;
 
+	StartupInfo.cb = sizeof(STARTUPINFO);
+	StartupInfo.lpReserved = NULL;
+	StartupInfo.lpDesktop = NULL;
+	StartupInfo.lpTitle = NULL;
+	StartupInfo.dwFlags = STARTF_USESHOWWINDOW;
+	StartupInfo.wShowWindow = SW_SHOWNORMAL;
+	StartupInfo.cbReserved2 = 0;
+	StartupInfo.lpReserved2 = NULL;
 
-char cmd[256];
-strcpy_s(cmd, "quake2xp");
-strcat_s(cmd, " ");
+	char cmd[256];
+	strcpy_s(cmd, "quake2xp");
+	strcat_s(cmd, " ");
 
-for (int i = 1; i < argc; i++)
-{
-strcat_s(cmd, argv[i]);
-strcat_s(cmd, " ");
-}
+	for (int i = 1; i < argc; i++){
+		strcat_s(cmd, argv[i]);
+		strcat_s(cmd, " ");
+	}
 
 #ifdef UNICODE
 
-LPTSTR szCmdline = convertCharArrayToLPTSTR(cmd);
-CreateProcess(NULL, szCmdline, NULL, NULL, TRUE, 0, NULL, NULL, &StartupInfo, &ProcessInformation);
+	LPTSTR szCmdline = convertCharToLPTSTR(cmd);
+	CreateProcess(NULL, szCmdline, NULL, NULL, TRUE, 0, NULL, NULL, &StartupInfo, &ProcessInformation);
 
 #else
 
-CreateProcess(NULL, (LPSTR)cmd, NULL, NULL, TRUE, 0, NULL, NULL, &StartupInfo, &ProcessInformation);
+	CreateProcess(NULL, (LPSTR)cmd, NULL, NULL, TRUE, 0, NULL, NULL, &StartupInfo, &ProcessInformation);
 
 #endif
 
@@ -71,7 +77,7 @@ return 0;
 // win32 version non unicode only
 #ifndef CONSOLE
 
-#ifndef UNICODE 
+	#ifndef UNICODE 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
@@ -95,11 +101,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	strcat_s(cmd, " ");
 	strcat_s(cmd, lpCmdLine);
 
-	CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &StartupInfo, &ProcessInformation);
+	CreateProcess(NULL, (LPSTR)cmd, NULL, NULL, TRUE, 0, NULL, NULL, &StartupInfo, &ProcessInformation);
 
 
-	return 1;
+	return 0;
 }
 
-#endif
+	#endif
+
 #endif
