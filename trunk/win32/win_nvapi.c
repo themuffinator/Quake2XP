a719288544e6603da8aa7ab2963f155e98c94acb
kirkbarnes <kirkbarnes@users.sourceforge.net>
2021-01-24 19:48:49 +0000

    *nvapi update
    git-svn-id: https://svn.code.sf.net/p/quake2xp/code@1293 d83f5309-02f3-4a42-a5f4-12e6af65da40

--- trunk/win32/win_nvapi.c
+++ trunk/win32/win_nvapi.c
@@ -75,25 +75,12 @@ void GLimp_InitNvApi() {
 #define NV_UTIL_DOMAIN_VID  2 //video decoder don't needed
 #define NV_UTIL_DOMAIN_BUS  3
 
-#define		GPU_MEMORY_INFO_DEDICATED_VIDMEM_NVX          0x9047
-#define		GPU_MEMORY_INFO_TOTAL_AVAILABLE_MEMORY_NVX    0x9048
-#define		GPU_MEMORY_INFO_CURRENT_AVAILABLE_VIDMEM_NVX  0x9049
-#define		GPU_MEMORY_INFO_EVICTION_COUNT_NVX            0x904A
-#define		GPU_MEMORY_INFO_EVICTED_MEMORY_NVX            0x904B
-
 extern qboolean adlInit;
 
 void R_GpuInfo_f(void) {
 
-	NvAPI_Status					ret = NVAPI_OK;
-	NvU32							rpm = 0;
-	NvAPI_ShortString				string;
-	NV_GPU_THERMAL_SETTINGS			thermal;
-	NV_GPU_DYNAMIC_PSTATES_INFO_EX	m_DynamicPStateInfo;
-	NV_GPU_CLOCK_FREQUENCIES		clocks;
-
-	NV_GPU_PERF_PSTATES_INFO PerfPstatesInfo;
-	PerfPstatesInfo.version = NV_GPU_PERF_PSTATES_INFO_VER;
+	NvAPI_Status		ret = NVAPI_OK;
+	NvAPI_ShortString	string;
 
 	if (adlInit) {
 		ADL_PrintGpuInfo();
@@ -113,53 +100,71 @@ void R_GpuInfo_f(void) {
 		Com_Printf(S_COLOR_RED"...NvAPI_GPU_GetFullName() fail: %\n", string);
 	}
 	else
-		Com_Printf(S_COLOR_YELLOW"...Get GPU statistic from " S_COLOR_GREEN "%s\n", string);
+		Com_Printf("...Get GPU statistic from: " S_COLOR_GREEN "%s\n", string);
 
+	ret = NvAPI_GPU_GetVbiosVersionString(hPhysicalGpu[0], string);
+	if (ret != NVAPI_OK) {
+		NvAPI_GetErrorMessage(ret, string);
+		Com_Printf(S_COLOR_RED"...NvAPI_GPU_GetVbiosVersionString() fail: %\n", string);
+	}
+	else
+		Com_Printf("...Bios Version: " S_COLOR_GREEN "%s\n", string);
+	
 	for (int i = 0; i < physicalGpuCount; i++) {
 
 		Com_Printf("\n   GPU " S_COLOR_GREEN "%i" S_COLOR_WHITE ":\n", i);
-
+				
 		// get gpu temperature
+		NV_GPU_THERMAL_SETTINGS	thermal;
 		thermal.version = NV_GPU_THERMAL_SETTINGS_VER_2;
 		ret = NvAPI_GPU_GetThermalSettings(hPhysicalGpu[i], 0, &thermal);
-
 		if (ret != NVAPI_OK) {
 			NvAPI_GetErrorMessage(ret, string);
 			Com_Printf(S_COLOR_RED"...NvAPI_GPU_GetThermalSettings() fail: %\n", string);
 		}
 		else
-			Com_Printf("...temperature: " S_COLOR_GREEN "%u" S_COLOR_WHITE " Celsius (%s)\n", thermal.sensor[0].currentTemp,
-				GLimp_NvApi_GetThermalController(thermal.sensor[0].controller));
+			Com_Printf("...temperature: " S_COLOR_GREEN "%u" S_COLOR_WHITE " Celsius (%s)\n", thermal.sensor[i].currentTemp,
+				GLimp_NvApi_GetThermalController(thermal.sensor[i].controller));
 
 		// get fans speed
+		NvU32 rpm = 0;
 		ret = NvAPI_GPU_GetTachReading(hPhysicalGpu[i], &rpm);
-
-		if (ret != NVAPI_OK)
-		{
+		if (ret != NVAPI_OK) {
 			NvAPI_GetErrorMessage(ret, string);
 			Com_Printf(S_COLOR_RED"NvAPI_GPU_GetTachReading() fail: %s\n", string);
 		}
 		else
 			Com_Printf("...fan speed: " S_COLOR_GREEN "%u" S_COLOR_WHITE " rpm\n", rpm);
 
+		NvU32 pWidth;
+		uint pcieLines = 0;
+		ret = NvAPI_GPU_GetCurrentPCIEDownstreamWidth(hPhysicalGpu[i], &pWidth);
+		if (ret != NVAPI_OK) {
+			NvAPI_GetErrorMessage(ret, string);
+			Com_Printf(S_COLOR_RED"...NvAPI_GPU_GetCurrentPCIEDownstreamWidth() fail: %\n", string);
+		}
+		else
+			pcieLines = (uint)pWidth;
+
+		NV_GPU_DYNAMIC_PSTATES_INFO_EX	m_DynamicPStateInfo;
 		m_DynamicPStateInfo.version = NV_GPU_DYNAMIC_PSTATES_INFO_EX_VER;
 		ret = NvAPI_GPU_GetDynamicPstatesInfoEx(hPhysicalGpu[i], &m_DynamicPStateInfo);
-		if (ret != NVAPI_OK)
-		{
+		if (ret != NVAPI_OK){
 			NvAPI_GetErrorMessage(ret, string);
 			Com_Printf(S_COLOR_RED"NvAPI_GPU_GetDynamicPstatesInfoEx() fail: %s", string);
 		}
 		else
-			Com_Printf("...utilization: " S_COLOR_YELLOW "Core " S_COLOR_GREEN "%u" S_COLOR_WHITE " %%, " S_COLOR_YELLOW "Frame Buffer " S_COLOR_GREEN "%u" S_COLOR_WHITE " %%, " S_COLOR_YELLOW "PCIe Bus " S_COLOR_GREEN "%u" S_COLOR_WHITE " %%\n",
+			Com_Printf("...utilization: " S_COLOR_YELLOW "Core " S_COLOR_GREEN "%u" S_COLOR_WHITE " %%, " S_COLOR_YELLOW "Frame Buffer " S_COLOR_GREEN "%u" S_COLOR_WHITE " %%, " S_COLOR_YELLOW "PCIe x%i Bus " S_COLOR_GREEN "%u" S_COLOR_WHITE " %%\n",
 				m_DynamicPStateInfo.utilization[NV_UTIL_DOMAIN_GPU].percentage,
 				m_DynamicPStateInfo.utilization[NV_UTIL_DOMAIN_FB].percentage,
+				pcieLines,
 				m_DynamicPStateInfo.utilization[NV_UTIL_DOMAIN_BUS].percentage);
 		
 		// get gpu & vram frequencies
+		NV_GPU_CLOCK_FREQUENCIES clocks;
 		clocks.version = NV_GPU_CLOCK_FREQUENCIES_VER;
 		clocks.ClockType = NV_GPU_CLOCK_FREQUENCIES_CURRENT_FREQ;
 		ret = NvAPI_GPU_GetAllClockFrequencies(hPhysicalGpu[i], &clocks);
-
 		if (ret != NVAPI_OK) {
 			NvAPI_GetErrorMessage(ret, string);
 			Com_Printf(S_COLOR_RED "NvAPI_GPU_GetAllClockFrequencies() fail: %s\n", string);
@@ -167,25 +172,57 @@ void R_GpuInfo_f(void) {
 		else {
 			if (clocks.domain[NVAPI_GPU_PUBLIC_CLOCK_GRAPHICS].bIsPresent && clocks.domain[NVAPI_GPU_PUBLIC_CLOCK_MEMORY].bIsPresent)
 				Com_Printf("...frequencies: " S_COLOR_YELLOW "GPU: " S_COLOR_GREEN "%u" S_COLOR_WHITE " MHz " S_COLOR_YELLOW "VRAM: " S_COLOR_GREEN "%u" S_COLOR_WHITE " MHz\n",
-				(NvU32)((clocks.domain[NVAPI_GPU_PUBLIC_CLOCK_GRAPHICS].frequency + 500) / 1000),
+					(NvU32)((clocks.domain[NVAPI_GPU_PUBLIC_CLOCK_GRAPHICS].frequency + 500) / 1000),
 					(NvU32)((clocks.domain[NVAPI_GPU_PUBLIC_CLOCK_MEMORY].frequency + 500) / 1000));
 		}
 
-		NV_GPU_PERF_PSTATE_ID currentPState;
-		ret = NvAPI_GPU_GetCurrentPstate(hPhysicalGpu[i], &currentPState);
-		if (ret != NVAPI_OK)
-		{
+		// get performance states infos
+		NvU32  inputFlags = 1U;
+		NV_GPU_PERF_PSTATES_INFO PerfPstatesInfo;
+		PerfPstatesInfo.version = NV_GPU_PERF_PSTATES_INFO_VER;
+		ret = NvAPI_GPU_GetPstatesInfoEx(hPhysicalGpu[i], &PerfPstatesInfo, inputFlags);
+		if (ret != NVAPI_OK) {
 			NvAPI_GetErrorMessage(ret, string);
 			Com_Printf(S_COLOR_RED "NvAPI_GPU_GetCurrentPstate() fail: %s\n", string);
 		}
 		else
-			Com_Printf("\n...GPU Performance Level: " S_COLOR_GREEN "%i\n", (int)currentPState);
+			Com_Printf("\n...GPU Performance States: " S_COLOR_GREEN "%i\n", (int)PerfPstatesInfo.numPstates);
+		
+		NV_GPU_PERF_PSTATE_ID currentPState;
+		ret = NvAPI_GPU_GetCurrentPstate(hPhysicalGpu[i], &currentPState);
+		if (ret != NVAPI_OK) {
+			NvAPI_GetErrorMessage(ret, string);
+			Com_Printf(S_COLOR_RED "NvAPI_GPU_GetCurrentPstate() fail: %s\n", string);
+		}
+		else
+			Com_Printf("...Current GPU Performance Level: " S_COLOR_GREEN "%i\n", (int)currentPState);
+
+		// perf states speeds
+		NV_GPU_PERF_PSTATES20_INFO pStates2info = {0};
+		pStates2info.version = MAKE_NVAPI_VERSION(pStates2info, 2);
+		ret = NvAPI_GPU_GetPstates20(hPhysicalGpu[i], &pStates2info);
+		if (NVAPI_OK != ret) {
+			NvAPI_GetErrorMessage(ret, string);
+			Com_Printf(S_COLOR_RED "NvAPI_GPU_GetPstates20() fail: %s\n", string);
+		}
+		else{		
+			for (int z = 0; z < pStates2info.numPstates; z++) {
+
+				Com_Printf(S_COLOR_YELLOW"%s" S_COLOR_WHITE "P" S_COLOR_GREEN "%i" S_COLOR_WHITE ": " S_COLOR_YELLOW "GPU " S_COLOR_GREEN "%i" S_COLOR_WHITE "-" S_COLOR_GREEN "%i" S_COLOR_WHITE " MHz " S_COLOR_YELLOW "VRAM " S_COLOR_GREEN "%i" S_COLOR_WHITE "-" S_COLOR_GREEN "%i" S_COLOR_WHITE " MHz\n",
+					pStates2info.pstates[z].pstateId == currentPState ? ">" : " ",
+					(int)pStates2info.pstates[z].pstateId,
+					pStates2info.pstates[z].clocks[0].data.range.minFreq_kHz / 1000, //gpu
+					pStates2info.pstates[z].clocks[0].data.range.maxFreq_kHz / 1000,
+					pStates2info.pstates[z].clocks[1].data.range.minFreq_kHz / 1000, //vram
+					pStates2info.pstates[z].clocks[1].data.range.maxFreq_kHz / 1000);
+			}
+		
+		}
 
 		NV_DISPLAY_DRIVER_MEMORY_INFO gpuMemoryStatus;
 		gpuMemoryStatus.version = MAKE_NVAPI_VERSION(NV_DISPLAY_DRIVER_MEMORY_INFO_V2, 2);
 		ret = NvAPI_GPU_GetMemoryInfo(hPhysicalGpu[i], (NV_DISPLAY_DRIVER_MEMORY_INFO *)&gpuMemoryStatus);
-		if (NVAPI_OK != ret)
-		{
+		if (NVAPI_OK != ret) {
 			NvAPI_GetErrorMessage(ret, string);
 			Com_Printf(S_COLOR_RED "NvAPI_GPU_GetMemoryInfo() fail: %s\n", string);
 		} else{
